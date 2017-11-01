cbd69d27aced429f00a6eba92971d5aec5842643
Yary Ribero <yary.ribero@ignite.ee>
2017-11-01 17:53:32 +0200

    Added account functionalitites

--- conf.js
+++ conf.js
@@ -3,5 +3,11 @@
 
 // THIS IS A LIST OF PARAMETERS NEEDED IN THE CORE
 
+exports.deviceName = 'Dagcoin Core';
+exports.hub = 'testnetexplorer.dagcoin.org/wss/';
+exports.hub.bLight = true;
+exports.CONSOLIDATION_INTERVAL = 60 * 60 * 1000;
+
 exports.DAGCOIN_MESSAGE_TIMEOUT = 30 * 1000;
 exports.DB_READY_CHECK_INTERVAL = 10 * 1000;
+exports.MIN_PAYMENT_DELAY = 5 * 1000;
